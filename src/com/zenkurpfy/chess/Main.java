8b4b1e1cace79c4faed6750a4c06a9b744abf218
Kurpfy <zenkurpfy.pessoal@gmail.com>
2019-05-05 14:47:40 -0300

    Starting to implement Board and Piece

--- src/com/zenkurpfy/chess/Main.java
+++ src/com/zenkurpfy/chess/Main.java
@@ -1,8 +1,10 @@
 package com.zenkurpfy.chess;
 
+import com.zenkurpfy.chess.board.Board;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
     }
 }
---
+++ src/com/zenkurpfy/chess/board/Piece.java
@@ -0,0 +1,15 @@
+package com.zenkurpfy.chess.board;
+
+public class Piece {
+
+    protected Position position;
+    private final Board board;
+
+    public Piece(Board board) {
+        this.board = board;
+    }
+
+    protected Board getBoard() {
+        return board;
+    }
+}
